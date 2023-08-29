08bce7a9e7e887946167dcd1aaf36925ff0a951b
Emiel Wit <emiel.wit@bettyblocks.com>
2023-08-29 14:01:52 +0200

    feat: loop through array and collection

--- functions/loop/1.1/index.js
+++ functions/loop/1.1/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-await-in-loop */
-const loop = async ({ data = [] }, steps) => {
-  for (let index = 0; index < data.length; index += 1) {
-    await steps({ iterator: data[index], index });
+const loop = async ({ array = [] }, steps) => {
+  for (let index = 0; index < array.length; index += 1) {
+    await steps({ iterator: array[index], index });
   }
 };
 
